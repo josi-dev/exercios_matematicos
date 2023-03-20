10945f9ff8abcfb1e881bb87698c8d7212813ebf
josk8 <81787907+josk8@users.noreply.github.com>
2023-03-20 13:32:01 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
+let dadosMultiUm
+let dadosMultiDois
+let dadosDiviUm
+let dadosDiviDois
 
-function operacao(num1,num2) {
+
+
+function soma(num1,num2) {
     
     let desafio = document.getElementById("desafio").innerHTML = num1 + " + " + num2
     
     
     let opcao = document.getElementById("opcao").style.display = 'block'
     
-    let nivel = document.getElementById("nivel").style.display = 'none'
+    let nivelSoma = document.getElementById("nivelSoma").style.display = 'none'
         
-    
+    let nivelMenos = document.getElementById("nivelMenos").style.display = 'none'
         
+        let nivelMultiplicar = document.getElementById("nivelMultiplicar").style.display = 'none'
+    
+    let nivelDivisor = document.getElementById("nivelDivisor").style.display = 'none'
     
     
-    
-    
-    
-    
-    
+    document.querySelectorAll("p")[1].innerHTML = "+"
     
     let maca = "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpR6L3K5vIsga06fSkNH8AjU7QAKUQTqqVYA&usqp=CAU' width='60'>"
     
@@ -39,8 +44,6 @@ function operacao(num1,num2) {
      }
 
 
-
-
 }
 
 
@@ -48,10 +51,224 @@ function operacao(num1,num2) {
 
 
 
+
+
+
+
+function menos(num1,num2) {
+    
+    let desafio = document.getElementById("desafio").innerHTML = num1 + " - " + num2
+    
+    
+    let opcao = document.getElementById("opcao").style.display = 'block'
+    
+    let nivelSoma = document.getElementById("nivelSoma").style.display = 'none'
+    
+    let nivelMenos = document.getElementById("nivelMenos").style.display = 'none'
+    
+    let nivelMultiplicar = document.getElementById("nivelMultiplicar").style.display = 'none'
+    
+    let nivelDivisor = document.getElementById("nivelDivisor").style.display = 'none'
+    
+    
+    
+    
+    
+    
+    let caixa_esquerda = document.getElementById("img_esquerda")
+   
+       for (i = 0; i < num1; i++) {
+          let abacate = "<img src='https://cdn-icons-png.flaticon.com/512/2503/2503796.png' width='60' onclick='tirar(i)' >"
+           document.getElementById("img_esquerda").innerHTML = caixa_esquerda.innerHTML + abacate
+
+         console.log(i)
+       }
+    
+}
+
+
+
+
+
+
+
+
+function multiplicar(num1,num2) {
+    
+    let desafio = document.getElementById("desafio").innerHTML = num1 + " × " + num2
+    
+    let opcao = document.getElementById("opcao").style.display = 'block'
+    
+    let nivelSoma = document.getElementById("nivelSoma").style.display = 'none'
+        
+    let nivelMenos = document.getElementById("nivelMenos").style.display = 'none'
+    
+    let nivelMultiplicar = document.getElementById("nivelMultiplicar").style.display = 'none'
+    
+    let nivelDivisor = document.getElementById("nivelDivisor").style.display = 'none'
+    
+    
+    dadosMultiUm = num1
+    dadosMultiDois = num2
+    
+    
+    let banana = "<img src='https://cdn-icons-png.flaticon.com/512/6482/6482627.png' width='60'>"
+    
+    var complemento = 1
+    
+    for (i = 0; i < num2; i++) {
+       var caixa_esquerda =  document.getElementById("img_esquerda").innerHTML
+
+ document.getElementById("img_esquerda").innerHTML =caixa_esquerda + "<div style='border:1px solid purple;margin:2vw 1vw 0px 1vw;border-radius:4px;'></div>"
+       
+       }
+    
+    function adicionar() {
+           
+       
+      if(0 < num2) {
+        
+        for (i = 0; i < num1; i++) {
+            var box = document.querySelectorAll("div")[complemento].innerHTML
+            
+            document.querySelectorAll("div")[complemento].innerHTML = box + banana
+            
+            
+        }
+        ++complemento
+        
+    }
+    
+    
+    }
+    
+    var tempo = setInterval(function () {
+        adicionar()
+        if(complemento > num2) {
+        clearInterval(tempo)
+        console.log("entrou aqui")
+    }
+    },400)
+    
+    
+}
+
+
+
+
+
+
+
+
+function divisor(num1,num2) {
+    
+    let desafio = document.getElementById("desafio").innerHTML = num1 + " ÷ " + num2
+    
+    let desafioSinal = num1 + " / " + num2
+    
+    let opcao = document.getElementById("opcao").style.display = 'block'
+    
+    let nivelSoma = document.getElementById("nivelSoma").style.display = 'none'
+        
+    let nivelMenos = document.getElementById("nivelMenos").style.display = 'none'
+        
+        let nivelMultiplicar = document.getElementById("nivelMultiplicar").style.display = 'none'
+    
+    let nivelDivisor = document.getElementById("nivelDivisor").style.display = 'none'
+    
+    
+    for (i = 0; i < num2; i++) {
+        
+        var img_esquerda =  document.getElementById("img_esquerda").innerHTML
+        
+        document.getElementById("img_esquerda").innerHTML = img_esquerda + "<div style='border:1px solid purple;margin:2vw 1vw 0px 1vw;border-radius:4px;'></div>"
+        
+
+    }
+    
+    dadosDiviUm = num1
+    dadosDiviDois = num2
+    
+    var complemento = 1
+    var abacaxi = "<img src='https://cdn-icons-png.flaticon.com/512/6866/6866524.png' width='60'>"
+    
+    function adicionar() {
+           let desafioSinal = num1 + " / " + num2
+       
+      if(0 < num2) {
+        
+        for (i = 0; i < eval(desafioSinal); i++) {
+            var box = document.querySelectorAll("div")[complemento].innerHTML
+            
+            document.querySelectorAll("div")[complemento].innerHTML = box + abacaxi
+            
+            
+        }
+        ++complemento
+        
+    }
+    
+    
+    }
+    
+    
+    
+    var tempo = setInterval(function () {
+        adicionar()
+        if(complemento > num2) {
+        clearInterval(tempo)
+        console.log("entrou aqui")
+    }
+    },400)
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 function cal() {
-    var desafio = document.getElementById("desafio").innerHTML
+    let desafio = document.getElementById("desafio").innerHTML
     var resultado = document.getElementById("resultado").value
-    if(eval(desafio) == resultado) {
+    var testeMulti = dadosMultiUm + " × " + dadosMultiDois
+    var testeDivi = dadosDiviUm + " ÷ " + dadosDiviDois
+    
+    if(desafio == testeMulti) {
+    
+        
+        
+        var desafioMulti = dadosMultiUm + " * " + dadosMultiDois
+
+        
+        if(eval(desafioMulti) == resultado) {
+            document.write("<dialog open id='certo'><img src='https://vitapromotora.sistemayuppie.com.br/public/assets/sucess.gif' width='60%'></dialog>")
+        document.getElementById("certo").style.textAlign = 'center'
+document.getElementById("certo").style.border = 'none'
+        }else {
+            document.write("<dialog open id='certo'><img src='https://cdn.dribbble.com/users/251873/screenshots/9388228/error-img.gif' width='60%'></dialog>")
+        document.getElementById("certo").style.textAlign = 'center'
+document.getElementById("certo").style.border = 'none'
+        }
+        
+        
+        
+        
+        
+    }else if (desafio == testeDivi) {
+    
+    var desafioDivi = dadosDiviUm + " / " + dadosDiviDois
+    
+    
+    if(eval(desafioDivi) == resultado) {
         document.write("<dialog open id='certo'><img src='https://vitapromotora.sistemayuppie.com.br/public/assets/sucess.gif' width='60%'></dialog>")
         document.getElementById("certo").style.textAlign = 'center'
 document.getElementById("certo").style.border = 'none'
@@ -62,8 +279,34 @@ document.getElementById("certo").style.border = 'none'
     }
     
     
+    
+    
+    }else {
+        
+    
+    
+    
+    
+    if(eval(desafio) == resultado) {
+        document.write("<dialog open id='certo'><img src='https://vitapromotora.sistemayuppie.com.br/public/assets/sucess.gif' width='60%'></dialog>")
+        document.getElementById("certo").style.textAlign = 'center'
+document.getElementById("certo").style.border = 'none'
+    }else {
+        document.write("<dialog open id='certo'><img src='https://cdn.dribbble.com/users/251873/screenshots/9388228/error-img.gif' width='60%'></dialog>")
+        document.getElementById("certo").style.textAlign = 'center'
+document.getElementById("certo").style.border = 'none'
+    }
+    
+    }
+    
+}//fucao
+
+
+/* testes */
+var calculostestes = 1
+function tirar(posicao) {
+    var num = posicao - calculostestes
+    let imgOpa = document.querySelectorAll("img")[num].style.opacity = '0.2'
+     
+    calculostestes++
 }
-
-
-
-
